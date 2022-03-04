3fd90827d77a84d03562a841252df06e78d8b701
MitsuruFujiwara <fujiwara52jp@gmail.com>
2022-03-04 18:30:10 +0900

    add init.sh

---
+++ init.sh
@@ -0,0 +1,17 @@
+mkdir feats
+mkdir input
+mkdir output
+mkdir input/mens
+mkdir input/womens
+
+cd input/mens
+kaggle competitions download -c mens-march-mania-2022
+
+unzip '*.zip'
+rm *.zip
+
+cd ../womens
+kaggle competitions download -c womens-march-mania-2022
+
+unzip '*.zip'
+rm *.zip
