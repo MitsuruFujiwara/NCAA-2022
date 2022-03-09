b20f3ec7af645d2c4073faadf21bbe9b00b03863
MitsuruFujiwara <fujiwara52jp@gmail.com>
2022-03-09 15:20:47 +0900

    update preprocess results

--- src/001_results_mens.py
+++ src/001_results_mens.py
@@ -16,7 +16,7 @@ def main():
     TourneyCompactResults = pd.read_csv(f'{BASE_DIR}/MNCAATourneyCompactResults.csv')
     RegularSeasonCompactResults = pd.read_csv(f'{BASE_DIR}/MRegularSeasonCompactResults.csv')
     SecondaryTourneyCompactResults = pd.read_csv(f'{BASE_DIR}/MSecondaryTourneyCompactResults.csv')
-    MSampleSubmissionStage1 = pd.read_csv(f'{BASE_DIR}/MSampleSubmissionStage1.csv')
+    SampleSubmission = pd.read_csv(f'{BASE_DIR}/MSampleSubmissionStage1.csv')
 
     # drop unnecessary columns
     SecondaryTourneyCompactResults.drop('SecondaryTourney',axis=1,inplace=True)
@@ -27,11 +27,11 @@ def main():
     del TourneyCompactResults, RegularSeasonCompactResults, SecondaryTourneyCompactResults
 
     # drop unnecessary columns
-    df_w.drop('NumOT',axis=1,inplace=True)
+    df_w.drop(['NumOT','WLoc'],axis=1,inplace=True)
 
     # merge inverse data
     df_l = df_w.copy()
-    df_l.columns = ['Season', 'DayNum', 'LTeamID', 'LScore', 'WTeamID', 'WScore', 'WLoc']
+    df_l.columns = ['Season', 'DayNum', 'LTeamID', 'LScore', 'WTeamID', 'WScore']
     df = df_w.append(df_l)
 
     del df_w, df_l
@@ -39,11 +39,26 @@ def main():
     # add target
     df['target'] = df['WScore'] - df['LScore']
 
+    # drop score
+    df.drop(['WScore','LScore'],axis=1,inplace=True)
+
     # add ID
     df['ID'] = df['Season'].astype(str) +'_'+ df['WTeamID'].astype(str) +'_'+ df['LTeamID'].astype(str)
 
-    # to numeric
-    df['WLoc'] = df['WLoc'].map(DICT_LOC)
+    # split ID
+    SampleSubmission['Season'] = SampleSubmission['ID'].apply(lambda x: x[:4])
+    SampleSubmission['WTeamID'] = SampleSubmission['ID'].apply(lambda x: x[5:9])
+    SampleSubmission['LTeamID'] = SampleSubmission['ID'].apply(lambda x: x[10:14])
+
+    # add DayNum
+    SampleSubmission = SampleSubmission.merge(df[['ID','DayNum']],on='ID',how='left')
+
+    # add test flag
+    df['is_test'] = False
+    SampleSubmission['is_test'] = True
+
+    # merge 
+    df = df.append(SampleSubmission.drop('Pred',axis=1))
 
     # save pkl
     save2pkl('../feats/result_mens.pkl', df)
