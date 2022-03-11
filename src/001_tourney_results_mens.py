d61d53908521b83b38a0c940783a611179034101
MitsuruFujiwara <fujiwara52jp@gmail.com>
2022-03-11 10:00:15 +0900

    split tourney & season results

--- src/001_tourney_results_mens.py
+++ src/001_tourney_results_mens.py
@@ -7,40 +7,28 @@ from utils import save2pkl, line_notify
 from utils import BASE_DIR
 
 #==============================================================================
-# preprocess results mens
+# preprocess tourney results mens
 #==============================================================================
 
 def main():
 
     # load csv
     TourneyCompactResults = pd.read_csv(f'{BASE_DIR}/MNCAATourneyCompactResults.csv')
-    RegularSeasonCompactResults = pd.read_csv(f'{BASE_DIR}/MRegularSeasonCompactResults.csv')
-    SecondaryTourneyCompactResults = pd.read_csv(f'{BASE_DIR}/MSecondaryTourneyCompactResults.csv')
     SampleSubmission = pd.read_csv(f'{BASE_DIR}/MSampleSubmissionStage1.csv')
 
-    # drop unnecessary columns
-    SecondaryTourneyCompactResults.drop('SecondaryTourney',axis=1,inplace=True)
+    # split win & lose
+    df_w = TourneyCompactResults.copy()
+    df_l = TourneyCompactResults.copy()
 
-    # merge season & tourney
-    df_w = TourneyCompactResults.append(RegularSeasonCompactResults)
-    df_w = df_w.append(SecondaryTourneyCompactResults)
-    del TourneyCompactResults, RegularSeasonCompactResults, SecondaryTourneyCompactResults
+    # change column names
+    df_l.columns = ['Season', 'DayNum', 'LTeamID', 'LScore', 'WTeamID', 'WScore', 'WLoc', 'NumOT']
 
-    # drop unnecessary columns
-    df_w.drop(['NumOT'],axis=1,inplace=True)
-
-    # merge inverse data
-    df_l = df_w.copy()
-    df_l.columns = ['Season', 'DayNum', 'LTeamID', 'LScore', 'WTeamID', 'WScore', 'WLoc']
+    # merge
     df = df_w.append(df_l)
-
     del df_w, df_l
 
     # add target
-    df['target'] = df['WScore'] - df['LScore']
-
-    # drop score
-    df.drop(['WScore','LScore'],axis=1,inplace=True)
+    df['target'] = ((df['WScore']-df['LScore'])>0).astype(int)
 
     # add ID
     df['ID'] = df['Season'].astype(str) +'_'+ df['WTeamID'].astype(str) +'_'+ df['LTeamID'].astype(str)
@@ -54,11 +42,14 @@ def main():
     df['is_test'] = False
     SampleSubmission['is_test'] = True
 
+    # drop unnecessary columns
+    df.drop(['DayNum', 'WLoc', 'NumOT','WScore','LScore'],axis=1,inplace=True)
+
     # merge 
     df = df.append(SampleSubmission.drop('Pred',axis=1))
 
     # save pkl
-    save2pkl('../feats/result_mens.pkl', df)
+    save2pkl('../feats/tourney_result_mens.pkl', df)
 
     # LINE notify
     line_notify('{} done.'.format(sys.argv[0]))
